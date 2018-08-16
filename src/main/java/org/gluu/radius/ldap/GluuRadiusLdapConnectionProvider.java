023f6ccc2fa5dbdf6a39918107622f957f84556f
Dzouato Djeumen Rolain Bonaventure <uprightech@gmail.com>
2018-08-16 20:32:03 +0100

    Changes in this commit:   * Completed GluuRadiusLdapConnectionProvider (untested)

--- src/main/java/org/gluu/radius/ldap/GluuRadiusLdapConnectionProvider.java
+++ src/main/java/org/gluu/radius/ldap/GluuRadiusLdapConnectionProvider.java
@@ -4,8 +4,19 @@ import com.unboundid.ldap.sdk.LDAPInterface;
 import com.unboundid.ldap.sdk.LDAPConnection;
 import com.unboundid.ldap.sdk.LDAPConnectionOptions;
 import com.unboundid.ldap.sdk.LDAPConnectionPool;
+import com.unboundid.ldap.sdk.LDAPException;
+
+import com.unboundid.util.ssl.HostNameSSLSocketVerifier;
+import com.unboundid.util.ssl.SSLUtil;
+import com.unboundid.util.ssl.TrustAllSSLSocketVerifier;
+import com.unboundid.util.ssl.TrustStoreTrustManager;
+
+import java.security.GeneralSecurityException;
+import javax.net.SocketFactory;
+import javax.net.ssl.SSLSocketFactory;
 
 import org.gluu.radius.config.LdapConfiguration;
+
 
 
 public class GluuRadiusLdapConnectionProvider {
@@ -29,16 +40,15 @@ public class GluuRadiusLdapConnectionProvider {
 	}
 
 	private LdapConfiguration config;
-	private LDAPConnectionPool unboundconnections;
-	private LDAPConnectionPool boundconnections;
+	private LDAPConnectionPool unboundcp;
+	private LDAPConnectionPool boundcp;
 
 	public GluuRadiusLdapConnectionProvider(LdapConfiguration config) {
 
 		this.config = config;
-		this.unboundconnections = null;
-		this.boundconnections = null;
-		initUnboundConnPool();
-		initBoundConnPool();
+		this.unboundcp = createConnectionPool();
+		this.boundcp = createConnectionPool();
+		
 	}
 
 	public GluuRadiusLdapConnection getUnboundLdapConnection() {
@@ -51,22 +61,91 @@ public class GluuRadiusLdapConnectionProvider {
 		return null;	
 	}
 
-	private void initUnboundConnPool() {
+	
 
-		// initialize the pool of unbounded connections
-	}
+	private LDAPConnectionPool  createConnectionPool() {
 
-	private void initBoundConnPool() {
+		try {
 
-		// initilialize the pool of bounded connections
+			Integer connpoolsize = DEFAULT_CONN_POOL_SIZE;
+			if(config.getConnPoolSize() != null)
+				connpoolsize = config.getConnPoolSize();
+			LDAPConnection conn = null;
+			if(config.getSslEnabled() == true) {
+				conn = getSecuredLDAPConnection();
+			}else {
+				conn = getUnsecuredLDAPConnection();
+			}
+			return new LDAPConnectionPool(conn,connpoolsize);
+
+		}catch(LDAPException e) {
+			throw new GluuRadiusLdapException("Could not create connection pool",e);
+		}
 	}
 
 	private LDAPConnectionOptions getLdapConnectionOptions() {
 
 		LDAPConnectionOptions opts = new LDAPConnectionOptions();
-		
+
+		opts.setBindWithDNRequiresPassword(true);
+		opts.setFollowReferrals(false);
+
+		if(config.getSslEnabled() && config.getSslVerifyEnabled()) {
+			opts.setSSLSocketVerifier(new HostNameSSLSocketVerifier(true));
+		}else if(config.getSslEnabled() && !config.getSslVerifyEnabled()) {
+			opts.setSSLSocketVerifier(TrustAllSSLSocketVerifier.getInstance());
+		}
+
 		return opts;
 	}
 
+	private LDAPConnection getUnsecuredLDAPConnection() {
+
+		try {
+
+			LDAPConnectionOptions connopts = getLdapConnectionOptions();
+			LDAPConnection conn = new LDAPConnection(connopts,config.getHostname(),config.getPort());
+			return conn;
+		}catch(LDAPException e) {
+			throw new GluuRadiusLdapException("Could not create unsecured ldap connection",e);
+		}
+	}
+
+	private LDAPConnection getSecuredLDAPConnection() {
+
+		try {
+			LDAPConnectionOptions connopts = getLdapConnectionOptions();
+			LDAPConnection conn = new LDAPConnection(getSSLSocketFactory(),connopts,config.getHostname(),
+				config.getPort());
+			return conn;
+		}catch(LDAPException e) {
+			throw new GluuRadiusLdapException("Could not create secured ldap connection",e);
+		}
+	}
+
+	private SocketFactory getSSLSocketFactory() {
+
+		try {
+
+			if(config.getTrustStoreFile()==null)
+				return SSLSocketFactory.getDefault();
+
+			String truststorefile = config.getTrustStoreFile();
+			char [] truststorepin = null;
+			if(config.getTrustStorePin() != null)
+				truststorepin = config.getTrustStorePin().toCharArray();
+
+			String truststoreformat = config.getTrustStoreFormat();
+
+			TrustStoreTrustManager tmanager = new TrustStoreTrustManager(
+				config.getTrustStoreFile(),
+				truststorepin,truststoreformat,true);
+			SSLUtil sslutil = new SSLUtil(tmanager);
+			return sslutil.createSSLSocketFactory();
+
+		}catch(GeneralSecurityException e) {
+			throw new GluuRadiusLdapException("Could not initialize ssl");
+		}
+	}
 
 }
